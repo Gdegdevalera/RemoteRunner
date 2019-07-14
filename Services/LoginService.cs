2fdde74b8b3e22a582df98daa7568f6b9dc56eb5
Gdegdevalera <laval_alex@mail.ru>
2019-07-14 21:30:51 +0300

    Configuration

--- Services/LoginService.cs
+++ Services/LoginService.cs
@@ -1,31 +1,46 @@
 using System;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
+using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 
 namespace RemoteRunner.Services
 {
     public class LoginService
     {
-        public string Login(string userName, string password)
+        private readonly User[] _users;
+        public LoginService(IConfiguration configuration)
         {
-            if (userName != "test" && password != "test")
-                return null;
-
-            return CreateToken(userName);
+            _users = configuration.GetSection("Users").Get<User[]>();
         }
         
-        private string CreateToken(string userName)
+        public string Login(string userName, string password)
+        {
+            var user = _users.FirstOrDefault(x => x.Login == userName && x.Password == password);
+           
+            return user != null 
+                ? CreateToken(user) 
+                : null;
+        }
+        
+        private static string CreateToken(User user)
         {
             var credentials = new SigningCredentials(Constants.AuthKey, SecurityAlgorithms.HmacSha256);  
   
             var token = new JwtSecurityToken(Constants.AuthIssuer,  
                 Constants.AuthAudience,  
-                new [] { new Claim("sub", userName) },  
+                new [] { new Claim("sub", user.Login) },  
                 expires: DateTime.Now.AddMinutes(120),  
                 signingCredentials: credentials);  
   
             return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private class User
+        {
+            public string Login { get; set; }
+            public string Password { get; set; }
         }
     }
 }
--- Services/ScriptService.cs
+++ Services/ScriptService.cs
@@ -1,36 +1,53 @@
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 
 namespace RemoteRunner.Services
 {
     public class ScriptService
     {
+        private readonly Script[] _scripts;
+        
+        public ScriptService(IConfiguration configuration)
+        {
+            _scripts = configuration.GetSection("Scripts").Get<Script[]>();
+        }
+        
         public IEnumerable<string> Run(string token, string scriptName)
         {
-            ValidateToken(token);
+            if (!ValidateToken(token))
+            {
+                yield break;
+            }
+
+            var script = _scripts.FirstOrDefault(x => x.Name == scriptName);
+
+            if (script == null)
+            {
+                yield return "Script has not found!";
+                yield break;
+            }
             
-            int i = 0;
-            yield return scriptName;
-            yield return "line" + i++;
-            yield return "line" + i++;
-            yield return "line" + i++;
-            yield return "line" + i++;
-            yield return "line" + i++;
-            yield return "line" + i++;
-            yield return "line" + i++;
-            yield return "line" + i++;
-            yield return "line" + i++;
-            yield return "line" + i++;
-            yield return "line" + i++;
+            yield return script.Name;
+            yield return script.Command;
         }
 
-        private static void ValidateToken(string token)
+        private static bool ValidateToken(string token)
         {
             var tokenHandler = new JwtSecurityTokenHandler();
             var validationParameters = GetValidationParameters();
 
-            tokenHandler.ValidateToken(token, validationParameters, out var validatedToken);
+            try
+            {
+                tokenHandler.ValidateToken(token, validationParameters, out _);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
         }
 
         private static TokenValidationParameters GetValidationParameters()
@@ -44,6 +61,12 @@ namespace RemoteRunner.Services
                 ValidAudience = Constants.AuthAudience,
                 IssuerSigningKey = Constants.AuthKey
             };
+        }
+
+        private class Script
+        {
+            public string Name { get; set; }
+            public string Command { get; set; }
         }
     }
 }
